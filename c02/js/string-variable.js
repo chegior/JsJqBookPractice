1e5ec39862bd289394572b16aa287eb1b4da69ea
chegior <chegiorca@hotmail.com>
2019-10-27 11:05:26 -0600

    add boolean mistake when using getElementId

--- c02/js/string-variable.js
+++ c02/js/string-variable.js
@@ -2,11 +2,12 @@ var username;
 var message;
 
 username = 'Molly';
-message = 'See our upcoming range';
-
+//message = 'See our upcoming range';
+message = '<a href=\"sale.html\">25% off!</a>';
 var elName = document.getElementById('name');
 elName.textContent = username;
 
 var elNote = document.getElementById('note');
-elNote.textContent = message;
+//elNote.textContent = message;
+elNote.innerHTML = message;//using the html with a scape value
 console.log(username);
